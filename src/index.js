78df49127a26ab7489d473405760daabd46b0490
Soyas Limbu <soyaslimbu07@gmail.com>
2024-02-08 10:53:17 +1100

    clean up

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import app from './app.js';
-import sequelize from './utils/connection.js';
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 const host = '0.0.0.0';
 app.listen(port, host, async () => {
   try {
-    await sequelize.authenticate();
-    console.log('Connection has been established successfully.');
     /* eslint-disable no-console */
     console.log(`Listening: http://0.0.0.0:${port}`);
     /* eslint-enable no-console */
   } catch (error) {
-    console.error('Unable to connect to the database:', error);
+    console.error(error);
   }
 });
